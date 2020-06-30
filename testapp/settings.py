52f72e3713de4dfdda0ce5993fef4d12a3e30667
Eduardo Moreno <hmitsurugi@gmail.com>
2020-06-30 10:02:50 +0200

    PostgreSQL connection in settings.

--- testapp/settings.py
+++ testapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'testapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testapp',
+        'USER': 'testuser',
+        'PASSWORD': 'testpwd',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
