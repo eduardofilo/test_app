4e4798c0abf3faf18b0e032adc3a690c1d876bfa
Eduardo Moreno <hmitsurugi@gmail.com>
2020-07-01 15:42:35 +0200

    'term' field in Word must be unique.

--- dictionary/models.py
+++ dictionary/models.py
@@ -4,13 +4,14 @@ from django.utils.translation import ugettext as _
 
 # Word model
 class Word(models.Model):
-    term = models.CharField(_('palabra'), max_length=100, blank=False, null=False)
+    term = models.CharField(_('palabra'), max_length=100, blank=False, null=False, unique=True)
 
     def __str__(self):
         return self.term
 
     class Meta:
         ordering = ["term"]
+        verbose_name = _('término')
 
 # Acceptation model
 class Acceptation(models.Model):
